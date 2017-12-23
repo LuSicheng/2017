c27a0c0f9b8302a889a4f85b37e589580386811b
LuSicheng <1264929989@qq.com>
2017-12-23 15:58:24 +0800

    update

--- Group_Project/js/mini_music_player.js
+++ Group_Project/js/mini_music_player.js
@@ -11,6 +11,20 @@ function replay(){
 	var audio=document.getElementById('bg_music');
 	audio.currentTime=0;
 }
+function volumeUp(){
+	var audio=document.getElementById('bg_music');
+	var volume=audio.volume;
+	if(audio.volume<=0.9){
+		audio.volume+=0.1;
+	}
+}
+function volumeDown(){
+	var audio=document.getElementById('bg_music');
+	var volume=audio.volume;
+	if(audio.volume>=0.1){
+		audio.volume-=0.1;
+	}
+}
 function mOver(obj){
 	obj.style.color="#FF0000"
 }
